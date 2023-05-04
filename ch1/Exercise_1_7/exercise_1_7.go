e152b5126ae3dbdeb195c0b2d0056f9763d77307
Zmey56 <zmey56@gmail.com>
2023-05-04 12:32:04 +0400

    1.5. Fetching a URL

---
+++ ch1/Exercise_1_7/exercise_1_7.go
@@ -0,0 +1,38 @@
+//“Exercise 1.7:
+//The function call io.Copy(dst, src) reads from src
+//and writes to dst. Use it instead of ioutil.ReadAll to copy the response
+//body to os.Stdout without requiring a buffer large
+//enough to hold the entire stream. Be sure to check the error result of io.Copy.”
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			log.Fatal(err)
+			b, err := io.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+				os.Exit(1)
+			}
+			fmt.Printf("%s", b)
+		}
+		resp.Body.Close()
+
+	}
+}
